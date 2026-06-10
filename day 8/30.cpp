89bb3848f7662329156edac11fd1cbed52960195
Kartik Sharma <kartiksharma4888@gmail.com>
2026-06-10 10:30:12 +0530

    Added Day 8 solutions

---
+++ day 8/30.cpp
@@ -0,0 +1,20 @@
+/*Write a program to Print number triangle.
+1
+12
+123
+1234
+12345
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
---
+++ day 8/31.cpp
@@ -0,0 +1,21 @@
+/*Write a program to Print character triangle.
+A
+AB
+ABC
+ABCD
+ABCDE
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;j++){
+            cout<<char(65 + (j-1));
+        }
+        cout<<endl;
+    }
+    return 0;
+}
